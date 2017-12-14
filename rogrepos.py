fa86a72dd96b05802a5d1c08567389a236a66a09
Park Hyunwoo <ez@smartstudy.co.kr>
2017-12-14 10:44:20 +0900

    Add arguments

--- rogrepos.py
+++ rogrepos.py
@@ -6,11 +6,10 @@ import click
 import sys
 from rcfile import rcfile
 
-limit = 365 * 2
-
 
 # Code snippet from https://gist.github.com/everilae/9697228
 class ThreadedGenerator(object):
+    """Helps generators run parallel."""
     def __init__(self, iterator, **kwargs):
         self._iterator = iterator
         self._sentinel = object()
@@ -34,10 +33,13 @@ class ThreadedGenerator(object):
 
 
 def default(value, default):
+    """Return default value when given value is None."""
     return value if value else default
 
 
 def print_repo(repo):
+    """Print basic information of given repository."""
+
     print(repo.full_name)
 
     if repo.description:
@@ -51,11 +53,15 @@ def print_repo(repo):
 
 
 def delete_repo(repo):
+    """Delete given repository."""
+
     repo.delete()
     print('{} was deleted.\n'.format(repo.full_name))
 
 
-def get_repos(g):
+def gen_repos(g):
+    """Generator of repositories."""
+
     now = datetime.now()
     user = g.get_user()
 
@@ -68,7 +74,9 @@ def get_repos(g):
         yield repo
 
 
-def get_orgs(g):
+def gen_orgs(g):
+    """Generator of orgs."""
+
     user = g.get_user()
 
     for org in user.get_orgs():
@@ -78,34 +86,53 @@ def get_orgs(g):
 
 
 def get_total_repos(g):
+    """Calculate total repository count."""
+
     user = g.get_user()
-    total_repos = default(user.public_repos, 0) + default(user.total_private_repos, 0)
+    total_repos = \
+        default(user.public_repos, 0) + \
+        default(user.total_private_repos, 0)
 
     print('Retrieving organizations from GitHub...')
-    for org in get_orgs(g):
-        total_repos += default(org.public_repos, 0) + default(org.total_private_repos, 0)
+    for org in gen_orgs(g):
+        total_repos += \
+            default(org.public_repos, 0) + \
+            default(org.total_private_repos, 0)
 
     return total_repos
 
 
-def main():
-    args = rcfile('rogrepos')
-    g = Github(args['token'])
+@click.command()
+@click.option('--days', default=365, help='Find neglected repositories over given days.')
+@click.option('--token', envvar='token', help='GitHub token to be used.')
+def rogrepos(days, token):
+    """Rogrepos helps you to remove outdated GitHub repositories."""
+
+    if not token:
+        args = rcfile('rogrepos')
+        try:
+            token = args['token']
+        except KeyError:
+            print('GitHub token is missing. Quitting...')
+            return -1
+
+    g = Github(token)
 
     total_repos = get_total_repos(g)
-
-    print('Retrieving {} repositorie(s) from GitHub...'.format(total_repos))
+    print('Retrieving {} repositories from GitHub...'.format(total_repos))
 
     c = 0
-    for repo in ThreadedGenerator(get_repos, g=g):
+    for repo in ThreadedGenerator(gen_repos, g=g):
         c += 1
 
-        if repo.neglected_days < limit:
+        # We only care neglected repositories.
+        if repo.neglected_days < days:
             continue
 
         print('{} of {}'.format(c, total_repos))
         print_repo(repo)
 
+        # Ask twice when it's a source repository, not forked.
         if click.confirm('\tDo you really want to delete?', default=False):
             if not repo.fork:
                 if click.confirm('\tThis is not a forked project. Are you sure?', default=False):
@@ -119,4 +146,4 @@ def main():
 
 
 if __name__ == '__main__':
-    sys.exit(main())
+    sys.exit(rogrepos())
